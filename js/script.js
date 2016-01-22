96c01aeff1985c3a2a6658442373bb01120e0847
Darkstar93 <thomasturek93@gmail.com>
2016-01-21 22:39:20 -0700

    fizzbuzz code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-fqa
+// Fizbuzz code.
+
+//Count through numbers 1 to 100 and find the numbers divisivble by 3, 5, or both.
+for (i=1; i<=100; i++)
+
+//For each number, do the following check:
+
+//See if it is divisible by 3
+if (i % 3 === 0){
+	//If so: Psee if it is ALSO divisble by 5.
+	if(i % 5 === 0){
+		//See if it is divisible by 3 or 5 If so: Print Fizzbuzz.
+		console.log('FizzBuzz');
+	} else {
+		//If only dibisible by 3: Print Fizz
+		console.log('Fizz');
+	}
+}
+
+else if (i % 5 === 0) {
+	//See if it is divisible by 5
+	//If so: Print Buzz
+	console.log('Buzz');
+} else {
+	//See if it is divisible by 3 or 5
+	//If so: Print print number
+console.log(i);
+}
+
+
+
